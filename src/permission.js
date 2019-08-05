b47a388a592240a33af7ddef0005332608351c85
lihao <857697832@qq.com>
2019-08-05 10:27:04 +0800

    2019-08-05 上线2

--- src/permission.js
+++ src/permission.js
@@ -4,27 +4,30 @@ import NProgress from 'nprogress' // Progress 进度条
 import 'nprogress/nprogress.css'// Progress 进度条样式
 import { Message } from 'element-ui'
 import { getUserToken, getAccountToken } from '@/utils/auth' // 验权
+import { getUserPermissionMenu } from '@/api/login'
+
 
 const whiteList = ['/login', '/404', '/select_account'] // 不重定向白名单
-
+function mapRoutesName(routes) {
+  const routersCodes = routes.map(route => {
+    return route.menu_code
+  })
+  return routersCodes
+}
 /**
  * 获取用户信息、菜单路由
  */
 function getUserInfo(to, from, next) {
-  let roles
-  // store.dispatch('GetActivity')
-  store.dispatch('GetInfo').then(() => {
-      store.dispatch('GetMenu').then(menuCode => {
-        store.dispatch('GenerateRoutes', menuCode ).then(() => { // 生成可访问的路由表
-          router.addRoutes(store.getters.addRouters) // 动态添加可访问路由表
-          next({...to, replace: true}) // hack方法 确保addRoutes已完成
-        })
-      })
+  store.dispatch('GetUserAccountTypeList')
+  store.dispatch('GetInfo')
+    .then(() => getUserPermissionMenu())
+    .then(res => {
+      const code = mapRoutesName(res.data)
+      store.dispatch('GenerateRoutes', code)
     })
     .then(() => {
-      store.dispatch('GetUserAccountTypeList').then(() => {
-        next()
-      })
+      router.addRoutes(store.getters.addRouters) // 动态添加可访问路由表
+      next({...to, replace: true}) // hack方法 确保addRoutes已完成
     })
 }
 
